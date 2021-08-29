685e88b8789b065f93550d1d7331a83b85667481
rails project <prashantjaiswal647@gmail.com>
2021-08-29 15:47:13 +0530

    extract rundency and production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     
 
 
@@ -15,9 +17,6 @@ class ArticlesController < ApplicationController
 
 
     def edit
-
-       @article = Article.find(params[:id])
-
 
     end    
 
@@ -37,7 +36,7 @@ class ArticlesController < ApplicationController
 
     def update
 
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -49,11 +48,11 @@ class ArticlesController < ApplicationController
     end   
 
     def show
-      @article = Article.find(params[:id])
+      
     end
 
     def destroy
-        @article = Article.find(params[:id])
+      
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -61,6 +60,11 @@ class ArticlesController < ApplicationController
 
 
     private
+
+        def set_article
+             @article = Article.find(params[:id])     
+        end
+
         def article_params
             params.require(:article).permit(:title, :description)
         end
